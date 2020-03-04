33048fcd87a93243d7fef5615b25a783bf7db686
Danilo Santos <dsantosbfs@gmail.com>
2020-03-04 17:36:01 -0300

    feat(error handler): implement error handler middleware

--- backend/api/src/config/server.js
+++ backend/api/src/config/server.js
@@ -2,8 +2,9 @@
 
 const bodyParser = require('body-parser');
 const cors = require('./cors');
+const errorHandler = require('./error-handler.middleware');
 const express = require('express');
-const jwt = require('./jwt-middleware');
+const jwt = require('./jwt.middleware');
 const port = 8080;
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended : true }));
 server.use(cors);
 server.use(jwt());
+server.use(errorHandler);
 
 server.listen(port, () => {
     console.log(`Run on port number ${port}`);
